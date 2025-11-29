b33cc7460756940aa75d997314b6fae93eb2e9bd
abdullahdude87-glitch <abdullah.dude87@gmail.com>
2025-11-30 00:50:57 +0500

    Update Counter2.tsx

--- src/app/Components/Counter/Counter2.tsx
+++ src/app/Components/Counter/Counter2.tsx
@@ -7,7 +7,7 @@ const Counter2 = () => {
           <div className="row cs_gap_y_30">
             <div className="col-lg-3 col-sm-6">
               <div className="cs_numberbox">
-                <div className="cs_counter_number cs_fs_74 cs_semibold cs_white_color cs_mb_10">
+                <div className="cs_counter_number cs_fs_50 cs_semibold cs_white_color cs_mb_10">
                   <span className="odometer" data-count-to="56">56</span>K+
                 </div>
                 <p className="cs_fs_18 cs_white_color mb-0">Customers visit app <br/> every months</p>
@@ -15,7 +15,7 @@ const Counter2 = () => {
             </div>
             <div className="col-lg-3 col-sm-6">
               <div className="cs_numberbox">
-                <div className="cs_counter_number cs_fs_74 cs_semibold cs_white_color cs_mb_10">
+                <div className="cs_counter_number cs_fs_50 cs_semibold cs_white_color cs_mb_10">
                   <span className="odometer" data-count-to="32">32</span>K+
                 </div>
                 <p className="cs_fs_18 cs_white_color mb-0">Total downloaded of <br/> our app</p>
@@ -23,7 +23,7 @@ const Counter2 = () => {
             </div>
             <div className="col-lg-3 col-sm-6">
               <div className="cs_numberbox">
-                <div className="cs_counter_number cs_fs_74 cs_semibold cs_white_color cs_mb_10">
+                <div className="cs_counter_number cs_fs_50 cs_semibold cs_white_color cs_mb_10">
                   <span className="odometer" data-count-to="156">156</span>K+
                 </div>
                 <p className="cs_fs_18 cs_white_color mb-0">Total Members of Relatix <br/> App Users</p>
@@ -31,7 +31,7 @@ const Counter2 = () => {
             </div>
             <div className="col-lg-3 col-sm-6">
               <div className="cs_numberbox">
-                <div className="cs_counter_number cs_fs_74 cs_semibold cs_white_color cs_mb_10">
+                <div className="cs_counter_number cs_fs_50 cs_semibold cs_white_color cs_mb_10">
                   <span className="odometer" data-count-to="42">42</span>+
                 </div>
                 <p className="cs_fs_18 cs_white_color mb-0">Satisfaction rate from our <br/> customers.</p>
@@ -43,5 +43,6 @@ const Counter2 = () => {
     </div>
     );
 };
+
 
 export default Counter2;
